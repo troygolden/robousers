75259952832b233284b664cf44fa93fe489cdd7f
troygolden <ts8769@gmail.com>
2019-12-11 13:09:11 -0500

    Updates

---
+++ precache-manifest.3333d6f13bf76c932cdc7b1f19ee74b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a2a17ff96ade972b73e2bb8d23c1e07",
+    "url": "/robousers/index.html"
+  },
+  {
+    "revision": "6037a7e6d7e39e24a5e2",
+    "url": "/robousers/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "48adacfa2fefbe8e6e1e",
+    "url": "/robousers/static/css/main.78144bf6.chunk.css"
+  },
+  {
+    "revision": "6037a7e6d7e39e24a5e2",
+    "url": "/robousers/static/js/2.54ad65e9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robousers/static/js/2.54ad65e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48adacfa2fefbe8e6e1e",
+    "url": "/robousers/static/js/main.1dba4c64.chunk.js"
+  },
+  {
+    "revision": "088124953812917df7ec",
+    "url": "/robousers/static/js/runtime-main.80d2fb72.js"
+  },
+  {
+    "revision": "ec2236662b6537d0f80b9b52affc4fb3",
+    "url": "/robousers/static/media/TRON.ec223666.woff"
+  }
+]);
